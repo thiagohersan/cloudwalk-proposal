95351e9a946669979834729776ff72e3d811cce0
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-02-24 14:45:24 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const images = [
+  "./imgs/landscape_00.jpg",
+  "./imgs/fire_00.jpg",
+  "./imgs/fire_01.jpg",
+  "./imgs/fire_02.jpg",
+  "./imgs/fire_03.jpg",
+];
+
+window.addEventListener('load', () => {
+  const container = document.getElementById('main-container');
+
+  images.forEach((imgPath) => {
+    const imgDivEl = document.createElement("div");
+    imgDivEl.classList.add("img-container");
+    container.appendChild(imgDivEl);
+
+    const imgEl = document.createElement("img");
+    imgEl.setAttribute("src", `${imgPath}`);
+    imgEl.classList.add("img-hor");
+    imgDivEl.appendChild(imgEl);
+  });
+});
